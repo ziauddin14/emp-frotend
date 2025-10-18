10b3ee9fc3200e23542425e3f1737fe591996f56
Zia Uddin <ziamaryam2526@gmail.com>
2025-10-18 21:58:08 +0500

    Update axiosConfig.js

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://emp-api-production-4d66.up.railway.app/",
+  baseURL: "https://emp-api-production-4d66.up.railway.app/api", 
 });
 
 export default axiosInstance;
