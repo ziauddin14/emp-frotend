b10fecd1b8abe582545fba0b58c4137f6c2dfee2
famiaattaria <famiaattaria1112@gmail.com>
2025-10-22 05:23:38 -0700

    fix: remove unused import and update axiosInstance import in DepartmentHelper

--- src/components/department/DepartmentHelper.jsx
+++ src/components/department/DepartmentHelper.jsx
@@ -1,7 +1,7 @@
-
+import React from "react";
 import axiosInstance from "../../axiosConfig";
-import { FaEdit, FaTrash } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
+import { FaEdit, FaTrash } from "react-icons/fa";
 
 export const columns = [
   {  
